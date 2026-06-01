6090827747d179ee704b809c944704150e8c1b9c
darielsantos-crypto <darielsantos@gmail.com>
2026-06-01 18:02:22 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-report-lucena-v17-pwa-v1';
+const CACHE_NAME = 'flash-report-lucena-v27-pwa-v1';
 const ASSETS = ['./', './index.html', './assets/manifest.webmanifest', './assets/icon-192.png', './assets/icon-512.png', './assets/apple-touch-icon.png', './assets/share-preview.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null)); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))); self.clients.claim(); });
